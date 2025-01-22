aeb1beb70330fcc66a0d8a7db3897df0b7b20255
IuliaSpataru <iulia.spataru@gmail.com>
2025-01-22 12:17:17 +0200

    Admin si Modele - Modificari 2

--- users/admin.py
+++ users/admin.py
@@ -1,29 +1,71 @@
 from django.contrib import admin
-
+from django.contrib.admin import SimpleListFilter
+from django.utils.html import format_html
+from django import forms
 from .models import Ward, User, Task, Patient
 
 
 # Register your models here.
 
+class WardAdminForm(forms.ModelForm):
+    class Meta:
+        model = Ward
+        fields = ['name', 'head_of_department', 'team_members']
+
+    def __init__(self, *args, **kwargs):
+        super(WardAdminForm, self).__init__(*args, **kwargs)
+        if 'instance' in kwargs:
+            ward = kwargs['instance']
+            self.fields['team_members'].queryset = User.objects.filter(ward=ward)
+
+
+
 
 @admin.register(Ward)
 class WardAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    form = WardAdminForm
+
+    list_display = ('name', 'head_of_department', )
+    search_fields = ('name',)
+    list_filter = ('name', 'head_of_department', 'team_members')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'head_of_department', 'team_members')
+        }),
+    )
+
+    def get_team_members(self, obj):
+        team_members = obj.team_members.all()
+        return ", ".join([user.name for user in team_members])
+
+    get_team_members.short_description = 'Team Members'
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'role', 'ward')
+    list_display = ('name', 'role', 'ward', 'head_of_department')
     list_filter = ('role', 'ward')
+    list_per_page = 15
+
+    def head_of_department(self, obj):
+        if obj.ward and obj.ward.head_of_department:
+            return obj.ward.head_of_department.name
+        return None
+
+    head_of_department.short_description = 'Head of Department'
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('description', 'user', 'completed', 'created_at')
     list_filter = ('completed', 'user__role', 'user__ward')
+    list_per_page = 5
 
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ward', 'created_at', 'updated_at')
+    list_display = ('name', 'ward', 'admitted_at', 'updated_at')
     list_filter = ('ward',)
+    list_per_page = 10
+
--- users/models.py
+++ users/models.py
@@ -5,6 +5,18 @@ from django.db.models import *
 
 class Ward(models.Model):
     name = models.CharField(max_length=128)
+    head_of_department = models.ForeignKey(
+        'User',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='head_of_department_of_ward'
+    )
+    team_members = models.ManyToManyField(
+        'User',
+        related_name='team_members_of_ward',
+        blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -34,14 +46,41 @@ class User(models.Model):
         return f"{self.name} - {self.get_role_display()}"
 
 
+class Patient(models.Model):
+    name = CharField(max_length=128)
+    ward = ForeignKey(Ward, on_delete=DO_NOTHING)
+    admitted_at = DateField(auto_now_add=True)
+    updated_at = DateField(auto_now=True)
+    room_number = IntegerField(primary_key=True, default=1)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Task(models.Model):
-    description = TextField()
-    user = ForeignKey(User, on_delete=CASCADE)
-    assigned_to = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
-    completed = BooleanField(default=False)
-    created_at = DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    user = models.ForeignKey(
+        'User',
+        on_delete=CASCADE,
+        related_name='tasks_created'
+    )
+    assigned_to = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    related_name='tasks_assigned'
+    )
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    patient = models.ForeignKey('Patient', on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
+    patient = models.ForeignKey(
+        'Patient',
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        null=True,
+        blank=True,
+    )
     room_number = models.CharField(max_length=10, blank=True, default='Pending')
 
     def save(self, *args, **kwargs):
@@ -54,12 +93,4 @@ class Task(models.Model):
         return f"Task for patient {self.patient.name}"
 
 
-class Patient(models.Model):
-    name = CharField(max_length=128)
-    ward = ForeignKey(Ward, on_delete=DO_NOTHING)
-    created_at = DateField(auto_now_add=True)
-    updated_at = DateField(auto_now=True)
-    room_number = IntegerField(primary_key=True, default=1)
 
-    def __str__(self):
-        return f"{self.name}"
